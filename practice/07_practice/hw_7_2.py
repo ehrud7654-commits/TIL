dc6409fa11de1398e0d202d8e91d9643821d21e6
ehrud7654 <ehrud7654@gmail.com>
2026-01-29 17:50:52 +0900

    nineth commit

--- practice/07_practice/hw_7_2.py
+++ practice/07_practice/hw_7_2.py
@@ -3,11 +3,22 @@ class StringRepeater:
 
     def __init__(self, number, string):
         self.number = number
-        self.number = string
+        self.string = string
 
-    def repeat_string()
+    def repeat_string(self):
+        for _ in range(self.number):
+            print(self.string)
     
 
 
-repeater1 = StringRepeater()
-repeater1.repeat_string(3, "Hello")
+repeater1 = StringRepeater(3, 'Hello')
+repeater1.repeat_string()
+
+
+# # 아래 클래스를 수정하시오.
+# class StringRepeater:
+#     pass
+
+
+# repeater1 = StringRepeater()
+# repeater1.repeat_string(3, "Hello")
